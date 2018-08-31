9a1a272dcbd46a591e0e550d9310e0f99c11a93e
Sagar Betkar <sagarbetkar1994@gmail.com>
2018-08-31 16:41:12 +0530

    Added spaceSchema

---
+++ models/space.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const spaceSchema = new Schema({
+  name: String,
+  slug: String,
+  anemities: Array,
+  size: Number,
+
+  address:{
+    line: Array,
+    area: String,
+    pincode: String,
+    city: String,
+    state: String,
+    country: String,
+    latitude: Number,
+    longitude: Number
+  },
+
+  timing: {
+    monday: {start: Date, end: Date},
+    tuesday: {start: Date, end: Date},
+    wednesday: {start: Date, end: Date},
+    thursday: {start: Date, end: Date},
+    friday: {start: Date, end: Date},
+    saturday: {start: Date, end: Date},
+    sunday: {start: Date, end: Date}
+  },
+
+  social: {
+    website: String,
+    facebook: String,
+    twitter: String
+  }
+});
+
+const Space = mongoose.model('Space', spaceSchema);
+
+module.exports = Space;
